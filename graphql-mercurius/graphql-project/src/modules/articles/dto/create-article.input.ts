128b4ee040583ce803e7e2a1b624eecc9630f0ac
tys-hiroshi <infinith4@gmail.com>
2024-05-22 08:11:09 +0900

    mod

--- graphql-mercurius/graphql-project/src/modules/articles/dto/create-article.input.ts
+++ graphql-mercurius/graphql-project/src/modules/articles/dto/create-article.input.ts
@@ -3,7 +3,7 @@ import { ID } from '@nestjs/graphql';
 import { ArticleStatus } from '../entities/articles-status.enum';
 import { users } from '../../users/entities/user.entity';
 import { Type } from 'class-transformer';
-import {usersCreateNestedOneWithoutArticlesInput} from '../../users/dto/users-create-nested-one-without-articles.input'
+// import {usersCreateNestedOneWithoutArticlesInput} from '../../users/dto/users-create-nested-one-without-articles.input'
 
 @InputType()
 export class CreateArticleInput {
