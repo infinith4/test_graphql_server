4074e22ea4ed36b48ef9149c64cdd3bdeca7600e
tys-hiroshi <infinith4@gmail.com>
2024-05-29 08:47:04 +0900

    Update auth.config.ts

--- nextjs-prisma-graphql-codegen-proj/nextjs-prisma-graphql-codegen-sample/auth.config.ts
+++ nextjs-prisma-graphql-codegen-proj/nextjs-prisma-graphql-codegen-sample/auth.config.ts
@@ -10,8 +10,8 @@ console.log(process.env.DB_HOST)
 
 // MySQLデータベースへの接続設定
 const db = mysql.createPool({
-  host: 'localhost',
-  port: 3306,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
