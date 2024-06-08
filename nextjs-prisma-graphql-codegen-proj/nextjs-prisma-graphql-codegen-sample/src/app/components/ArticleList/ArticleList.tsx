97772e7944ec664da524c0a4c6d29d201126be80
Hiroshi <infinith4@gmail.com>
2024-06-08 14:03:16 +0900

    Create ArticleList.tsx

---
+++ nextjs-prisma-graphql-codegen-proj/nextjs-prisma-graphql-codegen-sample/src/app/components/ArticleList/ArticleList.tsx
@@ -0,0 +1,29 @@
+import type { FC, FormEventHandler } from 'react'
+import { useEffect, useState } from 'react'
+
+import type { ArticlesQuery } from '@/generated/request'
+import {
+  useAddArticleMutation,
+  useArticlesQuery,
+  useDeleteArticleMutation,
+  useUpdateArticleMutation,
+} from '@/generated/request'
+
+export const ArticleList : FC = () => {
+  const [articleTitle, setArticleTitle ] = useState('')
+  const [articles, setArticles] = useState<ArticlesQuery['articles']>([])
+  const { loading, error, data } = useArticlesQuery()
+  const [addArticleMutation] = useAddArticleMutation()
+  const [updateArticleMutation] = useUpdateArticleMutation()
+  const [deleteArticleMutation] = useDeleteArticleMutation()
+
+  useEffect（effect: () :void  => {
+    setArticles(value: data?articles ?? [])
+  }, deps: [data?.articles])
+
+  if (loading) return <div>loading...</div>
+  if(error) return <div>error!</div>
+  if(!data?.articles) return null
+
+  return <div className="p-5 border rounded">article List</div>
+}
