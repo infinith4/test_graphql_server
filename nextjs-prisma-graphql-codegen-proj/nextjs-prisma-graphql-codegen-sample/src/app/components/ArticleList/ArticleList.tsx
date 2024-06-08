c0118a5ccfb480e2cffb8d612c2da2f4f59451c8
Hiroshi <infinith4@gmail.com>
2024-06-08 14:04:54 +0900

    Update ArticleList.tsx

--- nextjs-prisma-graphql-codegen-proj/nextjs-prisma-graphql-codegen-sample/src/app/components/ArticleList/ArticleList.tsx
+++ nextjs-prisma-graphql-codegen-proj/nextjs-prisma-graphql-codegen-sample/src/app/components/ArticleList/ArticleList.tsx
@@ -9,21 +9,21 @@ import {
   useUpdateArticleMutation,
 } from '@/generated/request'
 
-export const ArticleList : FC = () => {
-  const [articleTitle, setArticleTitle ] = useState('')
+export const ArticleList: FC = () => {
+  const [articleTitle, setArticleTitle] = useState('')
   const [articles, setArticles] = useState<ArticlesQuery['articles']>([])
   const { loading, error, data } = useArticlesQuery()
   const [addArticleMutation] = useAddArticleMutation()
   const [updateArticleMutation] = useUpdateArticleMutation()
   const [deleteArticleMutation] = useDeleteArticleMutation()
 
-  useEffect（effect: () :void  => {
-    setArticles(value: data?articles ?? [])
-  }, deps: [data?.articles])
+  useEffect((): void => {
+    setArticles(data?.articles ?? [])
+  }, [data?.articles])
 
   if (loading) return <div>loading...</div>
-  if(error) return <div>error!</div>
-  if(!data?.articles) return null
+  if (error) return <div>error!</div>
+  if (!data?.articles) return null
 
   return <div className="p-5 border rounded">article List</div>
 }
