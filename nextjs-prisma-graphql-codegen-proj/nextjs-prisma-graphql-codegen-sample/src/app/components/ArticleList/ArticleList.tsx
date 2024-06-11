27aaf8562018641351c348dd55cc557b9d8c0382
Hiroshi <infinith4@gmail.com>
2024-06-12 00:20:58 +0900

    Update ArticleList.tsx

--- nextjs-prisma-graphql-codegen-proj/nextjs-prisma-graphql-codegen-sample/src/app/components/ArticleList/ArticleList.tsx
+++ nextjs-prisma-graphql-codegen-proj/nextjs-prisma-graphql-codegen-sample/src/app/components/ArticleList/ArticleList.tsx
@@ -13,18 +13,20 @@ import {
 export const ArticleList: FC = () => {
   const [articleTitle, setArticleTitle] = useState('')
   const [articles, setArticles] = useState<ArticlesQuery['articles']>([])
-  const { loading, error, data } = useArticlesQuery()
-  const [addArticleMutation] = useAddArticleMutation()
-  const [updateArticleMutation] = useUpdateArticleMutation()
-  const [deleteArticleMutation] = useDeleteArticleMutation()
+  console.log("-----------------------------articles");
+  console.log(articles);
+  //const { loading, error, data } = useArticlesQuery()
+  // const [addArticleMutation] = useAddArticleMutation()
+  // const [updateArticleMutation] = useUpdateArticleMutation()
+  // const [deleteArticleMutation] = useDeleteArticleMutation()
 
-  useEffect((): void => {
-    setArticles(data?.articles ?? [])
-  }, [data?.articles])
+  // useEffect((): void => {
+  //   setArticles(data?.articles ?? [])
+  // }, [data?.articles])
 
-  if (loading) return <div>loading...</div>
-  if (error) return <div>error! {error.message}</div>
-  if (!data?.articles) return null
+  // if (loading) return <div>loading...</div>
+  // if (error) return <div>error! {error.message}</div>
+  // if (!data?.articles) return null
 
   return <div className="p-5 border rounded">article List</div>
 }
