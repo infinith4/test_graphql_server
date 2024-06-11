1e4b45e18776f53e3361602babd290d3d563ec0f
Hiroshi <infinith4@gmail.com>
2024-06-12 00:15:45 +0900

    Update ArticleList.tsx

--- nextjs-prisma-graphql-codegen-proj/nextjs-prisma-graphql-codegen-sample/src/app/components/ArticleList/ArticleList.tsx
+++ nextjs-prisma-graphql-codegen-proj/nextjs-prisma-graphql-codegen-sample/src/app/components/ArticleList/ArticleList.tsx
@@ -1,6 +1,6 @@
 'use client'
 import type { FC, FormEventHandler } from 'react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import type { ArticlesQuery } from '@/generated/request'
 import {
