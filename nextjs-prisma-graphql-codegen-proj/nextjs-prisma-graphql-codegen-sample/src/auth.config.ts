0b7cef07e4619f32c4b5fb1535f908ceccb9270c
Hiroshi <infinith4@gmail.com>
2024-06-24 13:30:25 +0900

    login success

--- nextjs-prisma-graphql-codegen-proj/nextjs-prisma-graphql-codegen-sample/src/auth.config.ts
+++ nextjs-prisma-graphql-codegen-proj/nextjs-prisma-graphql-codegen-sample/src/auth.config.ts
@@ -1,61 +1,91 @@
-// import { PrismaAdapter } from '@next-auth/prisma-adapter'
-// import { PrismaClient } from '@prisma/client'
-// import mysql from 'mysql2/promise'
+// // import { PrismaAdapter } from '@next-auth/prisma-adapter'
+// // import { PrismaClient } from '@prisma/client'
+// // import mysql from 'mysql2/promise'
+// // import type { NextAuthConfig } from 'next-auth'
+// // import Credentials from 'next-auth/providers/credentials'
+// // import { resolve } from 'path'
+
+// // console.log('process.env.DB_HOST')
+// // console.log(process.env.DB_HOST)
+
+// // // MySQLデータベースへの接続設定
+// // const db = mysql.createPool({
+// //   host: process.env.DB_HOST,
+// //   port: Number(process.env.DB_PORT),
+// //   user: process.env.DB_USER,
+// //   password: process.env.DB_PASSWORD,
+// //   database: process.env.DB_NAME,
+// //   connectionLimit: 1, // 接続を張り続けるコネクション数を指定
+// //   namedPlaceholders: true, // 設定必須
+// // })
+// // export const authConfig: NextAuthConfig = {
+// //   providers: [
+// //     Credentials({
+// //       async authorize(credentials) {
+// //         await new Promise((resolve) => setTimeout(resolve, 5000))
+
+// //         const [rows] = await db.execute(
+// //           'SELECT * FROM users WHERE email = :email',
+// //           {
+// //             email: credentials.email,
+// //           },
+// //         )
+
+// //         console.log('rows')
+// //         console.log(rows)
+// //         if (Array.isArray(rows) && rows.length === 1) {
+// //           const user = rows[0]
+// //           console.log('user')
+// //           console.log(user)
+
+// //           // パスワードの検証（例：bcryptを使用）
+// //           const isValid = credentials.password === user.password // 仮の検証方法。適切にハッシュ化されたパスワードを使用すること。
+
+// //           if (isValid) {
+// //             return { id: user.id, name: user.username, email: user.email }
+// //           }
+// //         }
+// //         return null
+// //         const email = 'user@nextemail.com'
+// //         // return credentials.email === email && credentials.password === '123456'
+// //         //   ? { id: 'userId', email }
+// //         //   : null
+// //       },
+// //     }),
+// //   ],
+// //   pages: {
+// //     signIn: '/login',
+// //   },
+// //   callbacks: {
+// //     authorized({ auth, request: { nextUrl } }) {
+// //       const isLoggedIn = !!auth?.user
+// //       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard')
+// //       if (isOnDashboard) {
+// //         if (isLoggedIn) return true
+// //         return false
+// //       } else if (isLoggedIn) {
+// //         return Response.redirect(new URL('/dashboard', nextUrl))
+// //       }
+// //       return true
+// //     },
+// //   },
+// // }
+
 // import type { NextAuthConfig } from 'next-auth'
 // import Credentials from 'next-auth/providers/credentials'
-// import { resolve } from 'path'
 
-// console.log('process.env.DB_HOST')
-// console.log(process.env.DB_HOST)
-
-// // MySQLデータベースへの接続設定
-// const db = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   port: Number(process.env.DB_PORT),
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   connectionLimit: 1, // 接続を張り続けるコネクション数を指定
-//   namedPlaceholders: true, // 設定必須
-// })
 // export const authConfig: NextAuthConfig = {
 //   providers: [
 //     Credentials({
 //       async authorize(credentials) {
 //         await new Promise((resolve) => setTimeout(resolve, 5000))
-
-//         const [rows] = await db.execute(
-//           'SELECT * FROM users WHERE email = :email',
-//           {
-//             email: credentials.email,
-//           },
-//         )
-
-//         console.log('rows')
-//         console.log(rows)
-//         if (Array.isArray(rows) && rows.length === 1) {
-//           const user = rows[0]
-//           console.log('user')
-//           console.log(user)
-
-//           // パスワードの検証（例：bcryptを使用）
-//           const isValid = credentials.password === user.password // 仮の検証方法。適切にハッシュ化されたパスワードを使用すること。
-
-//           if (isValid) {
-//             return { id: user.id, name: user.username, email: user.email }
-//           }
-//         }
-//         return null
 //         const email = 'user@nextemail.com'
-//         // return credentials.email === email && credentials.password === '123456'
-//         //   ? { id: 'userId', email }
-//         //   : null
+//         return credentials.email === email && credentials.password === '123456'
+//           ? { id: 'userId', email }
+//           : null
 //       },
 //     }),
 //   ],
-//   pages: {
-//     signIn: '/login',
-//   },
 //   callbacks: {
 //     authorized({ auth, request: { nextUrl } }) {
 //       const isLoggedIn = !!auth?.user
@@ -69,16 +99,21 @@
 //       return true
 //     },
 //   },
+//   pages: {
+//     signIn: '/login',
+//   },
 // }
 
 import type { NextAuthConfig } from 'next-auth'
 import Credentials from 'next-auth/providers/credentials'
+import { resolve } from 'path'
 
 export const authConfig: NextAuthConfig = {
   providers: [
     Credentials({
       async authorize(credentials) {
         await new Promise((resolve) => setTimeout(resolve, 5000))
+
         const email = 'user@nextemail.com'
         return credentials.email === email && credentials.password === '123456'
           ? { id: 'userId', email }
@@ -86,6 +121,9 @@ export const authConfig: NextAuthConfig = {
       },
     }),
   ],
+  pages: {
+    signIn: '/login',
+  },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user
@@ -98,8 +136,5 @@ export const authConfig: NextAuthConfig = {
       }
       return true
     },
-  },
-  pages: {
-    signIn: '/login',
   },
 }
--- nextjs-prisma-graphql-codegen-proj/nextjs-prisma-graphql-codegen-sample/src/auth.ts
+++ nextjs-prisma-graphql-codegen-proj/nextjs-prisma-graphql-codegen-sample/src/auth.ts
@@ -1,21 +1,26 @@
-// import NextAuth from 'next-auth'
+// // import NextAuth from 'next-auth'
 
-// import { authConfig } from '../auth.config'
+// // import { authConfig } from '../auth.config'
 
-// export const { handlers, auth, signIn, signOut } = NextAuth(authConfig)
+// // export const { handlers, auth, signIn, signOut } = NextAuth(authConfig)
 
-import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
+// import NextAuth from "next-auth";
+// import GitHub from "next-auth/providers/github";
 
-export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [GitHub],
-  callbacks: {
-    authorized({ request, auth }) {
-      const { pathname } = request.nextUrl;
-      if (pathname === "/dashboard") {
-        return !!auth;
-      }
-      return true;
-    },
-  },
-});
+// export const { handlers, auth, signIn, signOut } = NextAuth({
+//   providers: [GitHub],
+//   callbacks: {
+//     authorized({ request, auth }) {
+//       const { pathname } = request.nextUrl;
+//       if (pathname === "/dashboard") {
+//         return !!auth;
+//       }
+//       return true;
+//     },
+//   },
+// });
+
+import NextAuth from 'next-auth'
+import { authConfig } from '@/auth.config'
+
+export const { signIn, signOut } = NextAuth(authConfig)
