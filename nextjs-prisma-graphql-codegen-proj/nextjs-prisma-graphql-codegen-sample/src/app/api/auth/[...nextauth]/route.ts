b6c14335e27342d91c40e17a5893253c59e46e92
Hiroshi <infinith4@gmail.com>
2024-06-29 07:46:32 +0900

    del

--- nextjs-prisma-graphql-codegen-proj/nextjs-prisma-graphql-codegen-sample/src/app/api/auth/[...nextauth]/route.ts
+++
@@ -1,9 +0,0 @@
-//https://authjs.dev/getting-started/installation
-
-import NextAuth from 'next-auth'
-
-import { options } from '@/app/options'
-
-const handler = NextAuth(options)
-
-export { handler as GET, handler as POST }
