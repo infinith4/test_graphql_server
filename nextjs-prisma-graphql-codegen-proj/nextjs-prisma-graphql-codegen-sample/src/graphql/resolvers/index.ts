c5cac887080a519ad10fa48d158ff3ffc9920a21
Hiroshi <infinith4@gmail.com>
2024-07-01 08:29:15 +0900

    Update index.ts

--- nextjs-prisma-graphql-codegen-proj/nextjs-prisma-graphql-codegen-sample/src/graphql/resolvers/index.ts
+++ nextjs-prisma-graphql-codegen-proj/nextjs-prisma-graphql-codegen-sample/src/graphql/resolvers/index.ts
@@ -1,14 +1,13 @@
 // import type { Resolvers } from '@/generated/resolvers-types'
 //import type { Resolvers } from '@/graphql/dist/graphql_codegen'
-//import type { Resolvers } from '@/graphql/dist/resolvers-types'
 // import { prisma } from '@/libs/prisma'
 // import { PrismaClient } from '@prisma/client'
 // import { Context } from '@apollo/client'
 // import { prisma } from '@/libs/prisma'
-
 import { PrismaClient } from '@prisma/client'
 
-import type { Resolvers } from '@/generated/resolvers-types'
+import type { Resolvers } from '@/graphql/dist/resolvers-types'
+// import type { Resolvers } from '@/generated/resolvers-types'
 import prisma from '@/libs/prisma'
 
 export const resolvers: Resolvers = {
