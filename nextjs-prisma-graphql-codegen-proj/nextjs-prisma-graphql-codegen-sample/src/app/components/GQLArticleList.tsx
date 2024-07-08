fd3a2f408760ac3fc198483a5db8c59718f4fde3
Hiroshi <infinith4@gmail.com>
2024-07-09 06:54:19 +0900

    Update GQLArticleList.tsx

--- nextjs-prisma-graphql-codegen-proj/nextjs-prisma-graphql-codegen-sample/src/app/components/GQLArticleList.tsx
+++ nextjs-prisma-graphql-codegen-proj/nextjs-prisma-graphql-codegen-sample/src/app/components/GQLArticleList.tsx
@@ -3,29 +3,26 @@ import { gql, useQuery } from '@apollo/client'
 import type { ArticlesQuery } from '@/graphql/dist/graphql_codegen'
 import { ArticlesDocument } from '@/graphql/dist/graphql_codegen'
 import { getClient } from '@/libs/client'
-
+import NextAuth from 'next-auth'
+import authConfig from '@/auth.config'
 export default async function GQLArticleList() {
   console.log('GQLArticleList----------------------------------------')
   // クエリ
-
+  const { auth } = NextAuth(authConfig)
+  const session = await auth()
+  console.log(session)
   console.log('TODO: context is null----------------------------------------')
   const { data: queryData, loading } = await getClient().query<ArticlesQuery>({
     query: ArticlesDocument,
   })
   console.log('queryData----------------------------------------')
   console.log(queryData)
-  const articles = [...queryData.articles]
+  // const articles = [...queryData.articles]
 
   return (
     <main>
       <div>list of articles</div>
-      {articles
-        .sort((a, b) => (a.createdAt > b.createdAt ? 1 : -1))
-        .map((article) => (
-          <div key={article.id}>
-            <h1>{article.content}</h1>
-          </div>
-        ))}
+
     </main>
   )
 }
