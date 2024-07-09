8ab06880d70ad30852102e80c60103b3008d9c64
Hiroshi <infinith4@gmail.com>
2024-07-09 23:12:14 +0900

    Update client.ts

--- nextjs-prisma-graphql-codegen-proj/nextjs-prisma-graphql-codegen-sample/src/libs/client.ts
+++ nextjs-prisma-graphql-codegen-proj/nextjs-prisma-graphql-codegen-sample/src/libs/client.ts
@@ -1,11 +1,32 @@
-import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
+import { ApolloClient, HttpLink, InMemoryCache, createHttpLink } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context';
+import authConfig from '@/auth.config'
+import NextAuth from 'next-auth'
+
+const authLink = setContext(async (_, { headers }: { headers: Headers }) => {
+  const { auth } = NextAuth(authConfig)
+  const session = await auth()
+  // get the authentication token from local storage if it exists
+  const token = session?.token//window.localStorage.getItem('token');
+  console.log(`token-----------${token}`)
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4210/api/graphql',
+  fetchOptions: { cache: 'no-store' }, // 一旦、キャッシュしない設定で逃げる
+});
+
 
 export const getClient = () => {
   return new ApolloClient({
-    cache: new InMemoryCache(),
-    link: new HttpLink({
-      uri: 'http://localhost:4210/api/graphql', // 各自の環境で書き換えてください
-      fetchOptions: { cache: 'no-store' }, // 一旦、キャッシュしない設定で逃げる
-    }),
-  })
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache()
+  });
 }
